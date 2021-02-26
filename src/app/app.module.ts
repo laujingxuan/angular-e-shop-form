313858b76d9ec1712a29a6b914dfdfc078e6c583
jingxuan94 <E0639666@u.nus.edu>
2021-02-26 17:57:42 +0800

    delete unused details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './components/items.component';
-import { DetailsComponent } from './components/details.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemsComponent,
-    DetailsComponent
   ],
   imports: [
     BrowserModule,
